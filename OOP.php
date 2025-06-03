212ac89bc3fc37abaa13d735950e8a759874d066
Pedro r Leal <pedro08leal@gmail.com>
2025-06-03 10:13:43 -0300

    teste
    teste

---
+++ OOP.php
@@ -0,0 +1,17 @@
+<?php
+class Animal {
+    public function falar() {
+        return "Algum som";
+    }
+}
+
+class Cachorro extends Animal {
+    public function falar() {
+        return "Latido";
+    }
+}
+
+$cao = new Cachorro();
+echo $cao->falar(); // Latido
+
+?>
